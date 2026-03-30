18f58ea90a8d9d9ccc265a61fa1600be2d976e32
Mateus <mateus3.carvalho@alunos.ifsuldeminas.edu.br>
2026-03-30 09:44:12 -0300

    Aula 3

--- aula 3/exemplo_continue.py
+++ aula 3/exemplo_continue.py
@@ -1,13 +1,13 @@
 dict_alunos1 = {'nome': 'Giovanna', 'situacao':'Matriculado','nota':'0'}
 dict_alunos2 = {'nome': 'Gabriel Marques', 'situacao':'Evadido','nota':'0'}
-dict_alunos3 = {'nome': 'Giovanna', 'situacao':'Matriculado','nota':'0'}
+dict_alunos3 = {'nome': 'Evandro', 'situacao':'Matriculado','nota':'0'}
 
 lista_aluno = [dict_alunos1, dict_alunos2, dict_alunos3]
 
 for aluno in lista_aluno:
     if aluno["situacao"] != 'Matriculado':
         continue
-    nota = float(input(f'Digite a nota do aluno {aluno['nome']}'))
+    nota = float(input(f'Digite a nota do aluno {aluno['nome']}: '))
     aluno ['nota'] = nota
 
-    print(lista_aluno)
+print(lista_aluno)
